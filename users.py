2a9047c1ac6cb897004ac8e309b4f93f6379ae9b
Rano-9 <esamatti.karvonen@pispalanit.fi>
2024-10-03 16:53:05 +0300

    Lisätty tilojen poisto ja palautus mahdollisuus adminille. Lisätty admin funktioita. Tehty tilojen haltijat tietokanta.

--- users.py
+++ users.py
@@ -4,7 +4,7 @@ from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 def login(user, sana):
-    sql = f"SELECT id, password FROM users WHERE username = '{user}'"
+    sql = f"SELECT id, password, admin FROM users WHERE username = '{user}'"
     result = db.session.execute(text(sql))
     user = result.fetchone()
 
@@ -13,20 +13,36 @@ def login(user, sana):
     else:
         if check_password_hash(user.password,sana):
             session["user_id"] = user.id
+            if user.admin == True:
+                session["admin"] = True
+            else:
+                session["admin"] = False
             return True
         else:
             return False
+        
 def logout():
 
     try:
         del session["user_id"]
+        del session["admin"]
         return True
     except:
         return False
+    
 def register(user,sana):
     salattu = generate_password_hash(sana)
+    if user == "Rano":
+        sql = f"SELECT id FROM users WHERE username = 'Rano'"
+        result = db.session.execute(text(sql))
+        if result.fetchone():
+            return False
+        else:
+            sql = f"INSERT INTO users (username,password,admin) VALUES ('{user}','{salattu}',True)"
+            db.session.execute(text(sql))
+            db.session.commit()
     try:
-        sql = f"INSERT INTO users (username,password) VALUES ('{user}','{salattu}')"
+        sql = f"INSERT INTO users (username,password,admin) VALUES ('{user}','{salattu}',False)"
         db.session.execute(text(sql))
         db.session.commit()
 
@@ -35,4 +51,7 @@ def register(user,sana):
         return False
     
 def onko():
-    return session["user_id"]
+    try:
+        return session["user_id"]
+    except:
+        return False
